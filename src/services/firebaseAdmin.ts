e345c9c4e903b6df881b7d7e50706ac1801f3454
Willians Ferreira <williansdesigner@hotmail.com>
2025-07-27 14:39:20 -0300

    ajustando json

--- src/services/firebaseAdmin.ts
+++ src/services/firebaseAdmin.ts
@@ -1,11 +1,13 @@
 // src/services/firebaseAdmin.ts
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}");
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 
